c29a20aa42e4c33d8124a6b1281beab8dc6b0191
loganmorin <loganmorin5@hotmail.com>
2025-02-24 11:16:29 -0500

    Cours 5 partie 2
    Avancement dans la démo

--- cours05/formulaires/bootstrap.js
+++ cours05/formulaires/bootstrap.js
@@ -2,10 +2,34 @@ document.addEventListener('DOMContentLoaded',()=>{
     const form=document.getElementById('myForm');
 
     form.addEventListener('submit', evt=>{
-        if(!form.checkValidity()){
-            evt.preventDefault();
+        evt.preventDefault();
             evt.stopPropagation();
+        if(!form.checkValidity()){
+            
         }
+
+        form.classList.add('was-validated');
+
+        const champMdp=document.getElementById('password');
+        const champMdp2=document.getElementById('confirmPassword');
+        if(champMdp.value !== champMdp2.value){
+            champMdp2.classList.remove('is-valid');
+            champMdp2.classList.add('is-invalid');
+            champMdp2.focus();
+            return;
+        }else{
+            champMdp2.classList.remove('is-invalid');
+            champMdp2.classList.add('is-valid');
+        }
+        
+        let data=new FormData(form);
+        for(const [cle,valeur] of data.entries()){
+            console.log(cle, "=", valeur);
+        }
+
+        form.reset();
+        form.classList.remove('was-validated');
+        return;
     });
 }
 );
